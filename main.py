6a129e03051584b8b4a608b9dcdaba65896e5937
Rhuan Gonzaga <62728188+Rhuan-Gonzaga@users.noreply.github.com>
2021-05-26 14:53:49 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import json
+import requests
+
+
+lista = list()
+
+def requisicao():
+    r = requests.get("https://api.hgbrasil.com/weather?woeid=90200707")
+    rq = json.loads(r.text)
+    dadosreq(rq)
+
+
+
+def dadosreq(rq):
+    lista.append(rq['results']['temp'])
+    lista.append(rq["results"]["date"])
+    lista.append(rq["results"]["time"])
+    lista.append(rq["results"]["description"])
+    lista.append(rq["results"]["city"])
+    mostrarterminal()
+    return lista
+
+
+def mostrarterminal():
+    print("=" * 30)
+    print("PREVISÃO DO RIO DE JANEIRO")
+    print("=" * 30)
+    print("\n")
+    print(f"Cidade: {lista[4]}")
+    print(f"A data de hoje: {lista[1]}")
+    print(f"Temperatura: {lista[0]}°")
+    if lista[2] < "12:00":
+        print(f"Horario: {lista[2]}AM")
+    else:
+        print(f"Horario: {lista[2]}PM")
+    print(f"O clima está: {lista[3]}")
+
+
+
+
+requisicao()
+
